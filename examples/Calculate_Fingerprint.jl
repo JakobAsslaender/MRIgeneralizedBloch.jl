2727db47b824db82768ff4a919d781089ea3fe00
JakobAsslaender <jakob.asslaender@nyumc.org>
2021-03-19 16:34:21 -0400

    Cleanup

--- examples/Calculate_Fingerprint.jl
+++ examples/Calculate_Fingerprint.jl
@@ -44,33 +44,6 @@ plot!(TR * 1:length(TRF), imag(s), label="imag(s)", legend=:topleft)
 
 
 ## Matrix Exp solvers
-TRF = [100e-6; control[1:end - 1,2]]
-TRF[733] /= 10
-α = [π; control[1:end - 1,1] .+ control[2:end,1]]
-ω1 = α ./ TRF
-
-B1 = 1.2
-T2s = 10e-6
-Rrf_T = PreCompute_Saturation_gBloch(minimum(TRF), maximum(TRF), T2s, T2s, minimum(ω1), maximum(ω1), B1, B1)
-# Rrf_T = PreCompute_Saturation_Graham(minimum(TRF), maximum(TRF), T2s, T2s)
-
-s = MatrixApprox_calculate_magnetization(ω1, TRF, TR, ω0, B1, m0s, R1, R2f, Rx, T2s, Rrf_T)
-
-plot(s[1,:] ./ (1 - m0s), label="xf / m0f", legend=:topleft)
-plot!(s[2,:] ./ (1 - m0s), label="yf / m0f")
-plot!(s[3,:] ./ (1 - m0s), label="zf / m0f")
-plot!(s[4,:] ./ m0s, label="zs / m0s")
-
-##
-TRF = [100e-6; control[1:end - 1,2]]
-TRF .= 1e-3
-TRF[100:5:200] .= 50e-6
-α = [π; control[1:end - 1,1] .+ control[2:end,1]]
-α .= 0.8π
-ω1 = α ./ TRF
-
-B1 = 1.0
-T2s = 10e-6
 Rrf_gB = PreCompute_Saturation_gBloch(minimum(TRF), maximum(TRF), T2s, T2s, minimum(ω1), maximum(ω1), B1, B1)
 Rrf_Gr = PreCompute_Saturation_Graham(minimum(TRF), maximum(TRF), T2s, T2s)
 
@@ -85,4 +58,5 @@ plot!(sGr[1,:] ./ (1 - m0s), label="Gr xf")
 plot!(sGr[3,:] ./ (1 - m0s), label="Gr zf")
 plot!(sGr[4,:] ./ m0s, label="Gr zs")
 
+##
 plot(sgB[1,:] ./ sGr[1,:])
