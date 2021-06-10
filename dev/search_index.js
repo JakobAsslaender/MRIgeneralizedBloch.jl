235367378fd527ee57a5b50f9f43e49adee366fd
Documenter.jl <documenter@juliadocs.github.io>
2021-06-10 18:55:00 +0000

    build based on 1be92f6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MRIgeneralizedBloch","category":"page"},{"location":"#MRIgeneralizedBloch","page":"Home","title":"MRIgeneralizedBloch","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MRIgeneralizedBloch.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MRIgeneralizedBloch]","category":"page"}]
+}
