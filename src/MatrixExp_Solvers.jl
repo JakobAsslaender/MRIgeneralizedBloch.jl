d9c7ba372eeef8dbbdd54e1c470224ad94b5312b
JakobAsslaender <jakob.asslaender@nyumc.org>
2021-06-15 17:28:29 -0400

    First working re-work of matixExp solvers; transposed the resulting matrices

--- src/MatrixExp_Solvers.jl
+++ src/MatrixExp_Solvers.jl
@@ -9,10 +9,10 @@ function MatrixApprox_calculate_magnetization(ω1, TRF, TR, ω0, B1, m0s, R1, R2
 end
 
 function MatrixApprox_calculate_magnetization(ω1, TRF, TR, ω0, B1, m0s, R1, R2f, Rx, R2s_vec, sweep_phase::Vector{T}=[0]) where {T <: Number}
-    M = similar(ω1, 5, length(sweep_phase) * length(ω1))
+    M = similar(ω1, length(sweep_phase) * length(ω1), 5)
 
     for i in eachindex(sweep_phase)
-        Mi = @view M[:, (i - 1) * length(ω1) + 1:i * length(ω1)]
+        Mi = @view M[(i - 1) * length(ω1) + 1:i * length(ω1), :]
         MatrixApprox_calculate_magnetization!(Mi, ω1, TRF, TR, sweep_phase[i], ω0, B1, m0s, R1, R2f, Rx, R2s_vec)
     end
     return M
@@ -25,7 +25,7 @@ function MatrixApprox_calculate_magnetization(ω1, TRF, TR, ω0, B1, m0s, R1, R2
 end
 
 function MatrixApprox_calculate_magnetization(ω1, TRF, TR, ω0, B1, m0s, R1, R2f, Rx, R2s_vec, grad_list::Array{T,1}, sweep_phase=[0]) where {T <: grad_param}
-    M = similar(ω1, 5 * (1 + length(grad_list)), length(sweep_phase) * length(ω1))
+    M = similar(ω1, length(sweep_phase) * length(ω1), 5 * (1 + length(grad_list)))
 
     for i in eachindex(sweep_phase)
         Mi = @view M[:, (i - 1) * length(ω1) + 1:i * length(ω1)]
@@ -59,9 +59,9 @@ function MatrixApprox_calculate_signal(ω1, TRF, TR, ω0, B1, m0s, R1, R2f, Rx, 
 end
 
 function MatrixApprox_calculate_signal(ω1, TRF, TR, ω0, B1, m0s, R1, R2f, Rx, R2s_vec, grad_list::Array{T,1}, sweep_phase=[0]) where T <: grad_param
-    M = similar(ω1, ComplexF64, 1 + length(grad_list), length(sweep_phase) * length(ω1))
+    M = similar(ω1, ComplexF64, length(sweep_phase) * length(ω1), 1 + length(grad_list))
     for i in eachindex(sweep_phase)
-        Mi = @view M[:, (i - 1) * length(ω1) + 1:i * length(ω1)]
+        Mi = @view M[(i - 1) * length(ω1) + 1:i * length(ω1), :]
         MatrixApprox_calculate_magnetization!(Mi, ω1, TRF, TR, sweep_phase[i], ω0, B1, m0s, R1, R2f, Rx, R2s_vec, grad_list)
     end
     return M
@@ -71,50 +71,12 @@ end
 ############################################################################
 # actual solvers, which are implemented with in-place calcluation of M / S
 ############################################################################
-function MatrixApprox_calculate_magnetization!(M, ω1, TRF, TR, sweep_phase, ω0, B1, m0s, R1, R2f, Rx, R2s_vec)
-    
-    (_R2s, _dR2sdT2s, _dR2sdB1) = R2s_vec
+function MatrixApprox_calculate_magnetization!(M, ω1, TRF, TR, sweep_phase, ω0, B1, m0s, R1, R2f, Rx, R2s_vec, grad_list=undef)
 
-    sweep_phase += π
-    u_rot = @SMatrix [cos(sweep_phase) -sin(sweep_phase) 0 0 0 0;
-                      sin(sweep_phase)  cos(sweep_phase) 0 0 0 0;
-                                     0                0  1 0 0 0;
-                                     0                0  0 1 0 0;
-                                     0                0  0 0 1 0;
-                                     0                0  0 0 0 1]
-    
-    y0 = antiperiodic_boundary_conditions_linear(ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, [], u_rot)
+    y0 = antiperiodic_boundary_conditions_linear(ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, R2s_vec[1], R2s_vec[2], R2s_vec[3], sweep_phase, grad_list)
+    # ms_setindex!(M, y0, 1)
 
-    propagate_magnetization_linear!(M, ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, u_rot, y0, [])
-        
-    return M
-end
-
-function MatrixApprox_calculate_magnetization!(M, ω1, TRF, TR, sweep_phase, ω0, B1, m0s, R1, R2f, Rx, R2s_vec, grad_list)
-    
-    (_R2s, _dR2sdT2s, _dR2sdB1) = R2s_vec
-
-    sweep_phase += π
-    u_rot = @SMatrix [cos(sweep_phase) -sin(sweep_phase) 0 0 0 0                 0                0 0 0 0;
-                      sin(sweep_phase)  cos(sweep_phase) 0 0 0 0                 0                0 0 0 0;
-                      0                 0                1 0 0 0                 0                0 0 0 0;
-                      0                 0                0 1 0 0                 0                0 0 0 0;
-                      0                 0                0 0 1 0                 0                0 0 0 0;
-                      0                 0                0 0 0 cos(sweep_phase) -sin(sweep_phase) 0 0 0 0;
-                      0                 0                0 0 0 sin(sweep_phase)  cos(sweep_phase) 0 0 0 0;
-                      0                 0                0 0 0 0                 0                1 0 0 0;
-                      0                 0                0 0 0 0                 0                0 1 0 0;
-                      0                 0                0 0 0 0                 0                0 0 1 0;
-                      0                 0                0 0 0 0                 0                0 0 0 1]
-
-    nM = size(M, 1) ÷ (length(grad_list) + 1) # 1 component for signal, 5 components for magnetization (xf, yf, zf, xs, zs)
-    for i in eachindex(grad_list)
-        y0 = antiperiodic_boundary_conditions_linear(ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, grad_list[i], u_rot)
-
-        # pick first set of rows for the main magnetization and second one for the derivatives
-        Mig = @view M[[1:nM;i * nM + 1:(i + 1) * nM],:] 
-        propagate_magnetization_linear!(Mig, ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, u_rot, y0, grad_list[i])
-    end
+    propagate_magnetization_linear!(M, ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, R2s_vec[1], R2s_vec[2], R2s_vec[3], sweep_phase, y0, grad_list)
     return M
 end
 
@@ -122,9 +84,9 @@ end
 ############################################################################
 # helper functions
 ############################################################################
-function antiperiodic_boundary_conditions_linear(ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, grad, u_rot)
-    
+function antiperiodic_boundary_conditions_linear(ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, sweep_phase=0, grad=undef)
     # put inversion pulse at the end (this defines y0 as the magnetization at the first TE after the inversion pulse)
+    u_rot = z_rotation_propagator(sweep_phase, grad)
     u_fp = exp(hamiltonian_linear(0, B1, ω0, TR / 2, m0s, R1, R2f, Rx, _R2s[1], _dR2sdT2s[1], _dR2sdB1[1], grad))
     u_pl = propagator_linear_inversion_pulse(ω1[1], TRF[1], B1, _R2s[1], _dR2sdT2s[1], _dR2sdB1[1], grad)
     A = u_fp * u_pl * u_rot * u_fp
@@ -141,41 +103,78 @@ function antiperiodic_boundary_conditions_linear(ω1, B1, ω0, TRF, TR, m0s, R1,
     return y0
 end
 
-# TODO: remove T 
-function propagate_magnetization_linear!(M::AbstractArray{T,2}, ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, u_rot, y0, grad) where T <: Real
-    M[:,1] = @view y0[1:end - 1]
+function antiperiodic_boundary_conditions_linear(ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, sweep_phase, grad_list::Vector{<:grad_param})
+    y0 = similar(grad_list, Vector)
+    # y0 = Vector{Vector{Float64}}(undef, length(grad_list))
+    for j in eachindex(grad_list)
+        y0[j] = antiperiodic_boundary_conditions_linear(ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, sweep_phase, grad_list[j])
+    end
+    return y0
+end
+
+
+function propagate_magnetization_linear!(M, ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, sweep_phase, y0, grad=undef)
+
+    u_rot = z_rotation_propagator(sweep_phase, grad)
+
+    ms_setindex!(M, y0, 1)
     for i = 2:length(ω1)
         u_fp = exp(hamiltonian_linear(0, B1, ω0, (TR - TRF[i]) / 2, m0s, R1, R2f, Rx, _R2s[i], _dR2sdT2s[i], _dR2sdB1[i], grad))
         u_pl = exp(hamiltonian_linear(ω1[i], B1, ω0, TRF[i], m0s, R1, R2f, Rx, _R2s[i], _dR2sdT2s[i], _dR2sdB1[i], grad))
 
         y0 = u_fp * (u_pl * (u_rot * (u_fp * y0)))
-        M[:,i] = @view y0[1:end - 1]
+        ms_setindex!(M, y0, i)
     end
     return M
 end
 
-function propagate_magnetization_linear!(S::AbstractArray{T,1}, ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, u_rot, y0, grad) where T <: Complex
-    S[1] = y0[1] + 1im * y0[2]
-    for i = 2:length(ω1)
-        u_fp = exp(hamiltonian_linear(0, B1, ω0, (TR - TRF[i]) / 2, m0s, R1, R2f, Rx, _R2s[i], _dR2sdT2s[i], _dR2sdB1[i], grad))
-        u_pl = exp(hamiltonian_linear(ω1[i], B1, ω0, TRF[i], m0s, R1, R2f, Rx, _R2s[i], _dR2sdT2s[i], _dR2sdB1[i], grad))
+function propagate_magnetization_linear!(M, ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, u_rot, y0, grad_list::Vector{<:grad_param})
+    nM = size(M, 2) ÷ (length(grad_list) + 1) # 1 component for signal, 5 components for magnetization (xf, yf, zf, xs, zs)
 
-        y0 = u_fp * (u_pl * (u_rot * (u_fp * y0)))
-        S[i] = y0[1] + 1im * y0[2]
+    for j in eachindex(grad_list)
+        Mj = @view M[:, [1:nM; j * nM + 1:(j + 1) * nM]] # pick first set of rows for the main magnetization and second one for the derivatives
+        propagate_magnetization_linear!(Mj, ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, u_rot, y0[j], grad_list[j])
     end
+    return M
+end
+
+function ms_setindex!(M::AbstractArray{<:Real,2}, y, i)
+    M[i,:] = @view y[1:end - 1]
+    return M
+end
+function ms_setindex!(S::AbstractArray{<:Complex,1}, y, i)
+    S[i] = y[1] + 1im * y[2]
+    return S
+end
+function ms_setindex!(S::AbstractArray{<:Complex,2}, y, i)
+    S[i,1] = y[1] + 1im * y[2]
+    S[i,2] = y[6] + 1im * y[7]
     return S
 end
 
-function propagate_magnetization_linear!(S::AbstractArray{T,2}, ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, u_rot, y0, grad) where T <: Complex
-    S[1,1] = y0[1] + 1im * y0[2]
-    S[2,1] = y0[6] + 1im * y0[7]
-    for i = 2:length(ω1)
-        u_fp = exp(hamiltonian_linear(0, B1, ω0, (TR - TRF[i]) / 2, m0s, R1, R2f, Rx, _R2s[i], _dR2sdT2s[i], _dR2sdB1[i], grad))
-        u_pl = exp(hamiltonian_linear(ω1[i], B1, ω0, TRF[i], m0s, R1, R2f, Rx, _R2s[i], _dR2sdT2s[i], _dR2sdB1[i], grad))
+function z_rotation_propagator(sweep_phase, _)
+    sweep_phase += π
+    u_rot = @SMatrix [cos(sweep_phase) -sin(sweep_phase) 0 0 0 0;
+                      sin(sweep_phase)  cos(sweep_phase) 0 0 0 0;
+                                     0                0  1 0 0 0;
+                                     0                0  0 1 0 0;
+                                     0                0  0 0 1 0;
+                                     0                0  0 0 0 1]
+    return u_rot
+end
 
-        y0 = u_fp * (u_pl * (u_rot * (u_fp * y0)))
-        S[1,i] = y0[1] + 1im * y0[2]
-        S[2,i] = y0[6] + 1im * y0[7]
-    end
-    return S
+function z_rotation_propagator(sweep_phase, grad::grad_param)
+    sweep_phase += π
+    u_rot = @SMatrix [cos(sweep_phase) -sin(sweep_phase) 0 0 0 0                 0                0 0 0 0;
+                      sin(sweep_phase)  cos(sweep_phase) 0 0 0 0                 0                0 0 0 0;
+                      0                 0                1 0 0 0                 0                0 0 0 0;
+                      0                 0                0 1 0 0                 0                0 0 0 0;
+                      0                 0                0 0 1 0                 0                0 0 0 0;
+                      0                 0                0 0 0 cos(sweep_phase) -sin(sweep_phase) 0 0 0 0;
+                      0                 0                0 0 0 sin(sweep_phase)  cos(sweep_phase) 0 0 0 0;
+                      0                 0                0 0 0 0                 0                1 0 0 0;
+                      0                 0                0 0 0 0                 0                0 1 0 0;
+                      0                 0                0 0 0 0                 0                0 0 1 0;
+                      0                 0                0 0 0 0                 0                0 0 0 1]
+    return u_rot
 end
--- test/calculate_magnetization_n_signal.jl
+++ test/calculate_magnetization_n_signal.jl
@@ -29,10 +29,10 @@ m_gBloch = calculatemagnetization_gbloch_ide(ω1, TRF, TR, ω0, B1, m0s, R1, R2f
 ## test linear approximation 
 m_linapp = MatrixApprox_calculate_magnetization(ω1, TRF, TR, ω0, B1, m0s, R1, R2f, Rx, T2s, R2s_T)
 
-@test m_gBloch[1,:] ≈ m_linapp[1,:] rtol = 1e-3
-@test m_gBloch[2,:] ≈ m_linapp[2,:] atol = 1e-9
-@test m_gBloch[3,:] ≈ m_linapp[3,:] rtol = 1e-3
-@test m_gBloch[4,:] ≈ m_linapp[5,:] rtol = 1e-3
+@test m_gBloch[1,:] ≈ m_linapp[:,1] rtol = 1e-3
+@test m_gBloch[2,:] ≈ m_linapp[:,2] atol = 1e-9
+@test m_gBloch[3,:] ≈ m_linapp[:,3] rtol = 1e-3
+@test m_gBloch[4,:] ≈ m_linapp[:,5] rtol = 1e-3
 
 ## test Graham's solution
 m_Graham = calculatemagnetization_graham_ode(ω1, TRF, TR, ω0, B1, m0s, R1, R2f, Rx, T2s, [], 2)
@@ -45,6 +45,7 @@ m_Graham = calculatemagnetization_graham_ode(ω1, TRF, TR, ω0, B1, m0s, R1, R2f
 ## ##################################################################
 # signal functions
 #####################################################################
+println("w/o gradients:")
 print("Time to solve the full IDE:            ")
 s_gBloch = @btime calculatesignal_gbloch_ide(ω1, TRF, TR, ω0, B1, m0s, R1, R2f, Rx, T2s, 2)
 
@@ -55,7 +56,37 @@ print("Time to solve the linear approximation:")
 s_linapp = @btime MatrixApprox_calculate_signal(ω1, TRF, TR, ω0, B1, m0s, R1, R2f, Rx, T2s, R2s_T)
 
 @test real(s_gBloch) ≈ real(s_linapp) rtol = 1e-3
-@test imag(s_gBloch) ≈ imag(s_linapp) atol = 1e9
+@test imag(s_gBloch) ≈ imag(s_linapp) atol = 1e-9
 
 @test real(s_gBloch) ≈ real(s_Graham) rtol = 1e-2
 @test imag(s_gBloch) ≈ imag(s_Graham) atol = 1e-9
+
+println("with gradients:")
+grad_list = [grad_m0s(), grad_R1(), grad_R2f(), grad_Rx(), grad_T2s(), grad_ω0(), grad_B1()]
+
+print("Time to solve the full IDE:            ")
+s_gBloch = @time calculatesignal_gbloch_ide(ω1, TRF, TR, ω0, B1, m0s, R1, R2f, Rx, T2s, grad_list, 2)
+
+print("Time to solve Graham's approximation:  ")
+s_Graham = @time calculatesignal_graham_ode(ω1, TRF, TR, ω0, B1, m0s, R1, R2f, Rx, T2s, grad_list, 2)
+
+print("Time to solve the linear approximation:")
+s_linapp = MatrixApprox_calculate_signal(ω1, TRF, TR, ω0, B1, m0s, R1, R2f, Rx, T2s, R2s_T, grad_list)
+
+@test s_gBloch[1,:] ≈ s_linapp[:,1] rtol = 1e-3
+@test s_gBloch[2,:] ≈ s_linapp[:,2] rtol = 1e-3
+@test s_gBloch[3,:] ≈ s_linapp[:,3] rtol = 1e-3
+@test s_gBloch[4,:] ≈ s_linapp[:,4] rtol = 1e-3
+@test s_gBloch[5,:] ≈ s_linapp[:,5] rtol = 1e-3
+@test s_gBloch[6,:] ≈ s_linapp[:,6] rtol = 1e-2
+@test s_gBloch[7,:] ≈ s_linapp[:,7] rtol = 1e-3
+@test s_gBloch[8,:] ≈ s_linapp[:,8] rtol = 1e-3
+
+@test s_gBloch[1,:] ≈ s_Graham[1,:] rtol = 1e-1
+@test s_gBloch[2,:] ≈ s_Graham[2,:] rtol = 1e-1
+@test s_gBloch[3,:] ≈ s_Graham[3,:] rtol = 1e-1
+@test s_gBloch[4,:] ≈ s_Graham[4,:] rtol = 1e-1
+@test s_gBloch[5,:] ≈ s_Graham[5,:] rtol = 1e-1
+@test s_gBloch[6,:] ≈ s_Graham[6,:] rtol = 1e-1
+@test s_gBloch[7,:] ≈ s_Graham[7,:] rtol = 1e-1
+@test s_gBloch[8,:] ≈ s_Graham[8,:] rtol = 1e-1
