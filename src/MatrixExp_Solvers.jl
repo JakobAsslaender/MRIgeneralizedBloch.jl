f511568c5353e165b72b2dc570e1371393a3568f
JakobAsslaender <jakob.asslaender@nyumc.org>
2021-06-16 23:48:17 -0400

    implemented m0=:IR and m0=:thermal option

--- src/MatrixExp_Solvers.jl
+++ src/MatrixExp_Solvers.jl
@@ -4,40 +4,61 @@
 function calculatesignal_linearapprox(ω1, TRF, TR, ω0, B1, m0s, R1, R2f, Rx, R2s_vec; grad_list=[undef], sweep_phase=[0], m0=:antiperiodic, output=:complexsignal)
 
     if output == :complexsignal && grad_list == [undef]
-        M = Array{ComplexF64}(undef, length(ω1), length(sweep_phase), 1)
+        S = Array{ComplexF64}(undef, length(ω1), length(sweep_phase), 1)
         jj = 1
     elseif output == :complexsignal
-        M = Array{ComplexF64}(undef, length(ω1), length(sweep_phase), 1 + length(grad_list))
+        S = Array{ComplexF64}(undef, length(ω1), length(sweep_phase), 1 + length(grad_list))
         jj = [[1,j+1] for j=1:length(grad_list)]
     elseif output == :realmagnetization && grad_list == [undef]
-        M = Array{SVector{6,Float64}}(undef, length(ω1), length(sweep_phase), length(grad_list))
+        S = Array{SVector{6,Float64}}(undef, length(ω1), length(sweep_phase), length(grad_list))
         jj = 1
     elseif output == :realmagnetization
-        M = Array{SVector{11,Float64}}(undef, length(ω1), length(sweep_phase), length(grad_list))
+        S = Array{SVector{11,Float64}}(undef, length(ω1), length(sweep_phase), length(grad_list))
         jj = 1:length(grad_list)
     end
 
-    if m0 == :thermal && grad_list == [undef]
-        y0 = @SVector [0, 0, 1-m0s, 0, m0s, 1]
-    elseif m0 == :thermal
-        y0 = @SVector [0, 0, 1-m0s, 0, m0s, 0, 0, 0, 0, 0, 1]
-    elseif isa(m0, AbstractVector) && grad_list == [undef] && length(m0) < 6
-        y0 = SVector{6}([m0; zeros(5-length(m0)); 1])
-    elseif isa(m0, AbstractVector) && grad_list == [undef] && length(m0) < 10
-        y0 = SVector{11}([m0; zeros(10-length(m0)); 1])
+    if m0 == :thermal || m0 == :IR
+        if grad_list == [undef]
+            _m0 = @SVector [0, 0, 1-m0s, 0, m0s, 1]
+        else
+            _m0 = @SVector [0, 0, 1-m0s, 0, m0s, 0, 0, 0, 0, 0, 1]
+        end
+    elseif isa(m0, AbstractVector)
+        if grad_list == [undef] && length(m0) < 6
+            _m0 = SVector{6}([m0; zeros(5-length(m0)); 1])
+        elseif length(m0) < 11
+            _m0 = SVector{11}([m0; zeros(10-length(m0)); 1])
+        else
+            error("the m0 vector of length <= 5 w/o gradients or <= 10 with gradients")
+        end
     elseif m0 != :antiperiodic
-        error("m0 must either be :antiperiodic or a vector of length <= 5 w/o gradients or <= 10 with gradients")
+        error("m0 must either be :antiperiodic, :IR, :thermal, or a vector")
     end
 
     for j in eachindex(grad_list), i in eachindex(sweep_phase)
         if m0 == :antiperiodic
-            y0 = antiperiodic_boundary_conditions_linear(ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, R2s_vec[1], R2s_vec[2], R2s_vec[3], grad_list[j], sweep_phase[i])
+            m = antiperiodic_boundary_conditions_linear(ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, R2s_vec[1], R2s_vec[2], R2s_vec[3], grad_list[j], sweep_phase[i])
+        elseif m0 == :thermal || isa(m0, AbstractVector)
+            # this implements the α[1]/2 - TR/2 preparation (TR/2 is implemented in propagate_magnetization_linear!)
+            u_pr = exp(hamiltonian_linear(ω1[1]/2, B1, ω0, TRF[1], m0s, R1, R2f, Rx, R2s_vec[1][1], R2s_vec[2][1], R2s_vec[3][1], grad)) # R2sl is actually wrong for the prep pulse
+            m = u_pr * _m0
+        elseif m0 == :IR
+            # this implements the π - spoiler - TR - α[2]/2 - TR/2 preparation (TR/2 is implemented in propagate_magnetization_linear!)
+            u_ip = propagator_linear_inversion_pulse(ω1[1], TRF[1], B1, R2s_vec[1][1], R2s_vec[2][1], R2s_vec[3][1], grad)
+            m = u_ip * _m0
+            println(m)
+            u_fp = exp(hamiltonian_linear(0, B1, ω0, TR, m0s, R1, R2f, Rx, R2s_vec[1][1], R2s_vec[2][1], R2s_vec[3][1], grad))
+            m = u_fp * m
+            println(m)
+            u_pr = exp(hamiltonian_linear(ω1[2]/2, B1, ω0, TRF[2], m0s, R1, R2f, Rx, R2s_vec[1][2], R2s_vec[2][2], R2s_vec[3][2], grad)) # R2sl is actually wrong for the prep pulse
+            m = u_pr * m
+            println(m)
         end
 
-        Mij = @view M[:, i, jj[j]]
-        propagate_magnetization_linear!(Mij, ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, R2s_vec[1], R2s_vec[2], R2s_vec[3], sweep_phase[i], y0, grad_list[j])
+        Mij = @view S[:, i, jj[j]]
+        propagate_magnetization_linear!(Mij, ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, R2s_vec[1], R2s_vec[2], R2s_vec[3], sweep_phase[i], m, grad_list[j])
     end
-    return M
+    return S
 end
 
 function calculatesignal_linearapprox(ω1, TRF, TR, ω0, B1, m0s, R1, R2f, Rx, T2s, R2s_T; grad_list=[undef], sweep_phase=[0], m0=:antiperiodic, output=:complexsignal)
@@ -52,7 +73,7 @@ end
 # helper functions
 ############################################################################
 function antiperiodic_boundary_conditions_linear(ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, grad=undef, sweep_phase=0)
-    # put inversion pulse at the end (this defines y0 as the magnetization at the first TE after the inversion pulse)
+    # put inversion pulse at the end (this defines m as the magnetization at the first TE after the inversion pulse)
     u_rot = z_rotation_propagator(sweep_phase, grad)
     u_fp = exp(hamiltonian_linear(0, B1, ω0, TR / 2, m0s, R1, R2f, Rx, _R2s[1], _dR2sdT2s[1], _dR2sdB1[1], grad))
     u_pl = propagator_linear_inversion_pulse(ω1[1], TRF[1], B1, _R2s[1], _dR2sdT2s[1], _dR2sdB1[1], grad)
@@ -64,30 +85,30 @@ function antiperiodic_boundary_conditions_linear(ω1, B1, ω0, TRF, TR, m0s, R1,
         A = A * u_fp * u_pl * u_rot * u_fp
     end
 
-    y0 = eigvecs(A)[:,end]
-    y0 /= y0[end]
-    return y0
+    m = eigvecs(A)[:,end]
+    m /= m[end]
+    return m
 end
 
-function propagate_magnetization_linear!(M, ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, sweep_phase, y0, grad=undef)
+function propagate_magnetization_linear!(S, ω1, B1, ω0, TRF, TR, m0s, R1, R2f, Rx, _R2s, _dR2sdT2s, _dR2sdB1, sweep_phase, m, grad=undef)
 
     u_rot = z_rotation_propagator(sweep_phase, grad)
-
-    ms_setindex!(M, y0, 1, grad)
+    println(m)
+    ms_setindex!(S, m, 1, grad)
     for i = 2:length(ω1)
         u_fp = exp(hamiltonian_linear(0, B1, ω0, (TR - TRF[i]) / 2, m0s, R1, R2f, Rx, _R2s[i], _dR2sdT2s[i], _dR2sdB1[i], grad))
         u_pl = exp(hamiltonian_linear(ω1[i], B1, ω0, TRF[i], m0s, R1, R2f, Rx, _R2s[i], _dR2sdT2s[i], _dR2sdB1[i], grad))
 
-        y0 = u_fp * (u_pl * (u_rot * (u_fp * y0)))
+        m = u_fp * (u_pl * (u_rot * (u_fp * m)))
 
-        ms_setindex!(M, y0, i, grad)
+        ms_setindex!(S, m, i, grad)
     end
-    return M
+    return S
 end
 
-function ms_setindex!(M::AbstractArray{<:AbstractVector}, y, i, grad)
-    M[i] = y
-    return M
+function ms_setindex!(S::AbstractArray{<:AbstractVector}, y, i, grad)
+    S[i] = y
+    return S
 end
 function ms_setindex!(S::AbstractArray{<:Complex}, y, i, grad)
     S[i] = y[1] + 1im * y[2]
