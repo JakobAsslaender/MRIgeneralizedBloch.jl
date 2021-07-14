efc810e4a53bc9cfe940edb1bcbe906d9b70d86e
JakobAsslaender <jakob.asslaender@nyumc.org>
2021-07-14 16:06:21 -0400

    Try to fix CI

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,8 @@
 using Pkg
-Pkg.activate(".")
+Pkg.activate("docs")
+Pkg.develop(PackageSpec(path=pwd()))
+Pkg.instantiate()
+
 using MRIgeneralizedBloch
 using Documenter
 using Literate
@@ -42,10 +45,10 @@ Literate.markdown(FILE, OUTPUT)
 # Literate.notebook(FILE, OUTPUT, preprocess=notebook_filter)
 Literate.script(  FILE, OUTPUT)
 
-# FILE = joinpath(@__DIR__, "src/Analyze_NMR_Data.jl")
-# Literate.markdown(FILE, OUTPUT)
-# # Literate.notebook(FILE, OUTPUT, preprocess=notebook_filter)
-# Literate.script(  FILE, OUTPUT)
+FILE = joinpath(@__DIR__, "src/Analyze_NMR_Data.jl")
+Literate.markdown(FILE, OUTPUT)
+# Literate.notebook(FILE, OUTPUT, preprocess=notebook_filter)
+Literate.script(  FILE, OUTPUT)
 
 DocMeta.setdocmeta!(MRIgeneralizedBloch, :DocTestSetup, :(using MRIgeneralizedBloch); recursive=true)
 
@@ -67,7 +70,7 @@ makedocs(;
         "build_literate/Greens_functions.md",
         "build_literate/Simulation_ContinuousWave.md",
         "build_literate/Simulation_Pulse.md",
-        # "build_literate/Analyze_NMR_Data.md",
+        "build_literate/Analyze_NMR_Data.md",
         ],
         "API" => "api.md",
     ],
