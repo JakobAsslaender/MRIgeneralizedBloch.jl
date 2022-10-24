e3b5c47268d767cc8b9c3cc714dc277b103c1fa1
Documenter.jl <documenter@juliadocs.github.io>
2022-10-24 16:32:04 +0000

    build based on db0c5dd

---
+++ previews/PR24/build_literate/Analyze_NMR_PreSat_Data.jl
@@ -0,0 +1,109 @@
+Δ = exp.(range(log(0.01e3), log(100e3), length=20)) * 2π # rad/s
+
+ω1_dB = -60:5:-5 # dB
+ω1 = @. 10^(ω1_dB / 20) * π / 2 / 11.4e-6 # rad/s
+
+g_Lorentzian(Δ, T2) = T2 / π / (1 + (T2 * Δ)^2)
+g_Gaussian(Δ, T2) = T2 / sqrt(2π) * exp(-(T2 * Δ)^2 / 2)
+g_superLorentzian(Δ, T2) = T2 * sqrt(2 / π) * quadgk(ct -> exp(- 2 *  (T2 * Δ / (3 * ct^2 - 1))^2) / abs(3 * ct^2 - 1), 0, sqrt(1 / 3), 1)[1];
+
+using MRIgeneralizedBloch
+using QuadGK
+using LsqFit
+using LinearAlgebra
+using Statistics
+using Printf
+using Plots
+plotlyjs(bg=RGBA(31 / 255, 36 / 255, 36 / 255, 1.0), ticks=:native); # hide
+
+MnCl2_data(ω1_dB) = string("https://github.com/JakobAsslaender/MRIgeneralizedBloch_NMRData/blob/main/20210419_1mM_MnCl2/ja_PreSat_v2%20(", ω1_dB, ")/1/data.2d?raw=true")
+BSA_data(ω1_dB)   = string("https://github.com/JakobAsslaender/MRIgeneralizedBloch_NMRData/blob/main/20210416_15%25BSA_2ndBatch/ja_PreSat_v2%20(", ω1_dB, ")/1/data.2d?raw=true");
+
+include(string(pathof(MRIgeneralizedBloch), "/../../docs/src/load_NMR_data.jl"));
+
+x = zeros(Float64, length(ω1) * length(Δ), 2)
+x[:,1] = repeat(Δ, length(ω1))
+x[:,2] = vec(repeat(ω1, 1, length(Δ))');
+
+M = zeros(Float64, length(Δ), length(ω1))
+for i = 1:length(ω1_dB)
+    M[:,i] = load_first_datapoint(MnCl2_data(ω1_dB[i]); set_phase=:abs)
+end
+M ./= maximum(M);
+
+function single_compartment_model(x, p)
+    (m0, R1, T2) = p
+
+    Δ  = @view x[:,1]
+    ω1 = @view x[:,2]
+
+    Rrf = @. π * ω1^2 * g_Lorentzian(Δ, T2)
+    m = @. m0 * R1 / (R1 + Rrf)
+    return m
+end;
+
+R1 = 1.479 # 1/s
+T2 = 0.075 # s
+
+p = plot(xlabel="Δ [rad/s]", ylabel="M / max(M)", xaxis=:log, legend=:none)
+[scatter!(p, Δ, M[:,i], color=i) for i=1:length(ω1)]
+[plot!(p, Δ, reshape(single_compartment_model(x, [1,R1,T2]), length(Δ), length(ω1))[:,i], color=i) for i=1:length(ω1)]
+display(p)
+
+for i = 1:length(ω1_dB)
+    M[:,i] = load_first_datapoint(BSA_data(ω1_dB[i]); set_phase=:abs)
+end
+M ./= maximum(M);
+
+function Henkelman_model(x, p; lineshape=:superLorentzian)
+    (m0, m0s, R1f, R1s, T2f, T2s, Rx) = p
+
+    m0s /= 1 - m0s # switch from m0s + m0f = 1 to m0f = 1 normalization
+
+    Δ  = @view x[:,1]
+    ω1 = @view x[:,2]
+
+    Rrf_f = @. π * ω1^2 * g_Lorentzian(Δ, T2f)
+
+    if lineshape == :Lorentzian
+        Rrf_s = @. π * ω1^2 * g_Lorentzian(Δ, T2s)
+    elseif lineshape == :Gaussian
+        Rrf_s = @. π * ω1^2 * g_Gaussian(Δ, T2s)
+    elseif lineshape == :superLorentzian
+        Rrf_s = @. π * ω1^2 * g_superLorentzian(Δ, T2s)
+    end
+
+    m = @. m0 * (R1s * Rx * m0s + Rrf_s * R1f + R1f * R1s + R1f * Rx) / ((R1f + Rrf_f + Rx * m0s) * (R1s + Rrf_s + Rx) - Rx^2 * m0s)
+    return m
+end;
+
+p0   = [  1,0.01,   1,   5,0.052,  1e-5, 40]
+pmin = [  0,   0,   0,   0,0.052,  1e-6,  1]
+pmax = [Inf,   1, Inf, Inf,0.052, 10e-3,100];
+
+fit = curve_fit((x, p) -> Henkelman_model(x, p; lineshape=:superLorentzian), x, vec(M), p0, lower=pmin, upper=pmax)
+fit_std = stderror(fit)
+
+p = plot(xlabel="Δ [rad/s]", ylabel="M / max(M)", xaxis=:log, legend=:none)
+[scatter!(p, Δ, M[:,i], color=i) for i=1:length(ω1)]
+[plot!(p, Δ, reshape(Henkelman_model(x, fit.param), length(Δ), length(ω1))[:,i], color=i) for i=1:length(ω1)]
+display(p)
+
+fit = curve_fit((x, p) -> Henkelman_model(x, p; lineshape=:Lorentzian), x, vec(M), p0, lower=pmin, upper=pmax)
+fit_std = stderror(fit)
+
+p = plot(xlabel="Δ [rad/s]", ylabel="M / max(M)", xaxis=:log, legend=:none)
+[scatter!(p, Δ, M[:,i], color=i) for i=1:length(ω1)]
+[plot!(p, Δ, reshape(Henkelman_model(x, fit.param), length(Δ), length(ω1))[:,i], color=i) for i=1:length(ω1)]
+display(p)
+
+fit = curve_fit((x, p) -> Henkelman_model(x, p; lineshape=:Lorentzian), x, vec(M), p0, lower=pmin, upper=pmax)
+fit_std = stderror(fit)
+
+p = plot(xlabel="Δ [rad/s]", ylabel="M / max(M)", xaxis=:log, legend=:none)
+[scatter!(p, Δ, M[:,i], color=i) for i=1:length(ω1)]
+[plot!(p, Δ, reshape(Henkelman_model(x, fit.param), length(Δ), length(ω1))[:,i], color=i) for i=1:length(ω1)]
+display(p)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
---
+++ previews/PR24/build_literate/tutorial_singlepulse.jl
@@ -0,0 +1,122 @@
+using MRIgeneralizedBloch
+using DifferentialEquations
+using SpecialFunctions
+using QuadGK
+using Plots
+plotlyjs(bg = RGBA(31/255,36/255,36/255,1.0), ticks=:native); #hide
+
+m0s = 0.15
+R1f = 0.5 # 1/s
+R2f = 13 # 1/s
+R1s = 3 # 1/s
+T2s = 12e-6 # s
+Rx = 17; # 1/s
+
+B1 = 1
+ω0 = 0; # rad/s
+
+G = interpolate_greens_function(greens_superlorentzian, 0, 1000);
+
+α = π # rad
+TRF = 500e-6 # s
+ω1 = α/TRF # rad/s
+
+m0 = [1; 1];
+
+mfun(p, t) = m0;
+
+param = (ω1, B1, ω0, R1s, T2s, G) # defined by apply_hamiltonian_gbloch!
+prob = DDEProblem(apply_hamiltonian_gbloch!, m0, mfun, (0, TRF), param)
+z_gBloch = solve(prob)
+
+p = plot(z_gBloch, xlabel="t [s]", ylabel="zˢ(t)", idxs=1, label="g. Bloch")
+
+f_ω1(t) = ω1
+Rʳᶠ = graham_saturation_rate(ω0 -> lineshape_superlorentzian(ω0, T2s), f_ω1, TRF, ω0) # 1/s
+
+z_Graham(t) = (Rʳᶠ * exp(-t * (R1s + Rʳᶠ)) + R1s) / (R1s + Rʳᶠ)
+plot!(p, z_Graham, 0, TRF, label="Graham")
+
+z_gBloch(TRF)[1]
+
+z_Graham(TRF)
+
+m0 = [0; 0; 1-m0s; m0s; 1];
+
+param = (ω1, B1, ω0, m0s, R1f, R2f, Rx, R1s, T2s, G);
+
+prob = DDEProblem(apply_hamiltonian_gbloch!, m0, mfun, (0, TRF), param)
+m_gBloch = solve(prob)
+p = plot(m_gBloch, xlabel="t [s]", ylabel="m(t)", idxs=1:4, labels=["xᶠ" "yᶠ" "zᶠ" "zˢ"])
+
+TRF = 1e-3 # s
+NSideLobes = 1
+f_ω1(t) = sinc(2(NSideLobes+1) * t/TRF - (NSideLobes+1)) * α / (sinint((NSideLobes+1)π) * TRF/π / (NSideLobes+1))
+p = plot(f_ω1, 0, TRF, xlabel="t [s]", ylabel="ω₁(t)", labels=:none)
+
+quadgk(f_ω1, 0, TRF)[1] / α
+
+param = (f_ω1, B1, ω0, R1s, T2s, G)
+typeof(f_ω1) <: Function
+
+m0 = [1; 1]
+prob = DDEProblem(apply_hamiltonian_gbloch!, m0, mfun, (0, TRF), param)
+z_gBloch = solve(prob)
+p = plot(z_gBloch, xlabel="t [s]", ylabel="zˢ(t)", idxs=1, label="g. Bloch")
+
+Rʳᶠ = graham_saturation_rate(ω0 -> lineshape_superlorentzian(ω0, T2s), f_ω1, TRF, ω0)
+
+z_Graham(t) = (Rʳᶠ * exp(-t * (R1s + Rʳᶠ)) + R1s) / (R1s + Rʳᶠ)
+plot!(p, z_Graham, 0, TRF, label="Graham")
+
+z_gBloch(TRF)[1]
+
+z_Graham(TRF)
+
+param = (f_ω1, B1, ω0, m0s, R1f, R2f, Rx, R1s, T2s, G)
+m0 = [0; 0; 1-m0s; m0s; 1]
+prob = DDEProblem(apply_hamiltonian_gbloch!, m0, mfun, (0, TRF), param)
+m_gBloch = solve(prob)
+p = plot(m_gBloch, xlabel="t [s]", ylabel="m(t)", idxs=1:4, labels=["xᶠ" "yᶠ" "zᶠ" "zˢ"])
+
+TRF = 10.24e-3 # s
+γ = 267.522e6 # gyromagnetic ratio in rad/s/T
+ω₁ᵐᵃˣ = 13e-6 * γ # rad/s
+μ = 5 # shape parameter in rad
+β = 674.1 # shape parameter in 1/s
+
+f_ω1(t) = ω₁ᵐᵃˣ * sech(β * (t - TRF/2)) # rad/s
+f_ω0(t) = -μ * β * tanh(β * (t - TRF/2)) # rad/s
+f_φ(t)  = -μ * log(cosh(β * t) - sinh(β*t) * tanh(β*TRF/2)); # rad
+
+p = plot(f_ω1, 0, TRF, xlabel="t [s]", ylabel="ω₁(t) [rad/s]", labels=:none)
+
+p = plot(f_ω0, 0, TRF, xlabel="t [s]", ylabel="ω₀(t) [rad/s]", labels=:none)
+
+p = plot(f_φ, 0, TRF, xlabel="t [s]", ylabel="φ(t) [rad]", labels=:none)
+
+m0 = [0, 0, 1-m0s, m0s, 1]
+p = (f_ω1, B1, f_φ, m0s, R1f, R2f, Rx, R1s, T2s, G)
+m_gBloch = solve(DDEProblem(apply_hamiltonian_gbloch!, m0, mfun, (0, TRF), p))
+p = plot(m_gBloch, xlabel="t [s]", ylabel="m(t)", idxs=1:4, labels=["xᶠ" "yᶠ" "zᶠ" "zˢ"])
+
+Δω0 = 1000 # rad/s
+f_φ_or(t) = f_φ(t) + Δω0 * t; # rad
+
+B1 = 1.2 # 20% miss-calibration
+p = (f_ω1, B1, f_φ_or, m0s, R1f, R2f, Rx, R1s, T2s, G)
+m_gBloch = solve(DDEProblem(apply_hamiltonian_gbloch!, m0, mfun, (0, TRF), p))
+p = plot(m_gBloch, xlabel="t [s]", ylabel="m(t)", idxs=1:4, labels=["xᶠ" "yᶠ" "zᶠ" "zˢ"])
+
+ωx(t) = -B1 * f_ω1(t) * sin(f_φ_or(t))
+ωy(t) =  B1 * f_ω1(t) * cos(f_φ_or(t))
+zs_gBloch(t) = m_gBloch(t)[4]
+xs_gBloch(t) = quadgk(τ -> G((t - τ) / T2s) * ωx(τ) * zs_gBloch(τ), 0, t)[1]
+ys_gBloch(t) = quadgk(τ -> G((t - τ) / T2s) * ωy(τ) * zs_gBloch(τ), 0, t)[1];
+
+p = plot(xs_gBloch, 0, TRF, xlabel="t [s]", ylabel="m(t)", label="xˢ")
+plot!(p, ys_gBloch, 0, TRF, label="yˢ")
+plot!(p, zs_gBloch, 0, TRF, label="zˢ")
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
