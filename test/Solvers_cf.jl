a6eb80b7cb560ea7b6bdc3b80c20fec825994071
Sebastian Flassbeck <sebastian.flassbeck@nyulangone.org>
2025-07-25 16:12:51 -0400

    fix solvers test

--- test/Solvers_cf.jl
+++ test/Solvers_cf.jl
@@ -13,6 +13,7 @@ Npulse = 500
 # TRF .= 1e-3 # long TRF to make Graham and gBloch match better
 TRF = 100e-6 .+ 400e-6 * rand(Npulse)
 TRF[1] = 500e-6
+grad_moment = [:crusher; fill(:balanced, length(α)-1)]
 
 TR = 3.5e-3
 
@@ -29,14 +30,14 @@ Rex = 20
 ## gBloch model with IDE vs Graham vs linear approximation: complex signal
 s_gBloch_IDE    = calculatesignal_gbloch_ide(  α, TRF, TR, ω0, B1, m0s, R1f, R2f, Rex, R1s, T2s; Ncyc=4)
 s_Graham        = calculatesignal_graham_ode(  α, TRF, TR, ω0, B1, m0s, R1f, R2f, Rex, R1s, T2s; Ncyc=4)
-s_gBloch_linear = calculatesignal_linearapprox(α, TRF, TR, ω0, B1, m0s, R1f, R2f, Rex, R1s, T2s, R2slT)
+s_gBloch_linear = calculatesignal_linearapprox(α, TRF, TR, ω0, B1, m0s, R1f, R2f, Rex, R1s, T2s, R2slT; grad_moment)
 @test s_gBloch_IDE ≈ s_Graham        rtol = 5e-2
 @test s_gBloch_IDE ≈ s_gBloch_linear rtol = 5e-2
 
 ## gBloch model with IDE vs Graham vs linear approximation: magnetixation
 s_gBloch_IDE = calculatesignal_gbloch_ide(α, TRF, TR, ω0, B1, m0s, R1f, R2f, Rex, R1s, T2s; Ncyc=4, output=:realmagnetization)
 s_Graham     = calculatesignal_graham_ode(α, TRF, TR, ω0, B1, m0s, R1f, R2f, Rex, R1s, T2s; Ncyc=4, output=:realmagnetization)
-s_gBloch_linear = calculatesignal_linearapprox(α, TRF, TR, ω0, B1, m0s, R1f, R2f, Rex, R1s, T2s, R2slT; output=:realmagnetization)
+s_gBloch_linear = calculatesignal_linearapprox(α, TRF, TR, ω0, B1, m0s, R1f, R2f, Rex, R1s, T2s, R2slT; output=:realmagnetization, grad_moment)
 s_gBloch_linear_m = similar(s_gBloch_IDE)
 for i ∈ eachindex(s_gBloch_linear)
     s_gBloch_linear_m[i,:] = s_gBloch_linear[i][[1;2;3;5;6]]
