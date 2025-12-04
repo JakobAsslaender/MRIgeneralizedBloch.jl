099dcc1ab108f642de6e6d0cf1a5f7372986ce13
JakobAsslaender <jakob.asslaender@nyumc.org>
2025-12-04 15:53:51 -0500

    Port OCT gradients to Symbolics.jl

--- symbolic_derivatives/MatrixExp_Hamiltonian_Derivatives.jl
+++ symbolic_derivatives/MatrixExp_Hamiltonian_Derivatives.jl
@@ -7,15 +7,19 @@ using MRIgeneralizedBloch
 
 ##
 @variables ω1, B1, ω0, T, m0s, R1f, R2f, Rex, R1s, R1a, T2s, R2s, dR2sdT2s, dR2sdB1, grad_type
+@variables TRF, dR2sdB1, dR2sdω1, dR2sdTRF, dR2sdT2sdω1, dR2sdB1dω1, dR2sdT2sdTRF, dR2sdB1dTRF
 
-f_R2s(T2s, B1) = R2s
-@register_symbolic f_R2s(T2s, B1)
-@register_derivative f_R2s(T2s, B1) 1 SConst(dR2sdT2s)
-@register_derivative f_R2s(T2s, B1) 2 SConst(dR2sdB1)
+f_R2s(T2s, B1, ω1, TRF) = R2s
+@register_symbolic f_R2s(T2s, B1, ω1, TRF)
+@register_derivative f_R2s(T2s, B1, ω1, TRF) 1 SConst(dR2sdT2s)
+@register_derivative f_R2s(T2s, B1, ω1, TRF) 2 SConst(dR2sdB1)
 
-H = hamiltonian_linear(ω1, B1, ω0, T, m0s, R1f, R2f, Rex, R1s, f_R2s(T2s, B1))
+H = hamiltonian_linear(ω1, B1, ω0, T, m0s, R1f, R2f, Rex, R1s, f_R2s(T2s, B1, ω1, TRF))
+Z = zeros(Int, size(H))
 
-##
+## #########################################################################################
+# derivatives wrt. MT parameters (used for CRB calculations & NLLS fitting)
+############################################################################################
 fs_str = ""
 for p ∈ [m0s, R1f, R2f, Rex, R1s, T2s, B1, ω0, R1a]
     if isequal(p, R1a)
@@ -25,13 +29,12 @@ for p ∈ [m0s, R1f, R2f, Rex, R1s, T2s, B1, ω0, R1a]
     end
 
     dHdp = vcat(
-        hcat(H[1:end-1, 1:end-1], zeros(Int, size(H, 1) - 1, size(H, 2) - 1), H[1:end-1, end]),
+        hcat(H[1:end-1, 1:end-1], Z[1:end-1, 1:end-1], H[1:end-1, end]),
         hcat(Ḣ[1:end-1, 1:end-1], H[1:end-1, 1:end-1], Ḣ[1:end-1, end]),
         zeros(Int, 1, 2size(H, 2) - 1)
     )
 
-    dHdp = substitute(dHdp, Dict([f_R2s(T2s, B1) => R2s]))
-    dHdp = simplify.(dHdp)
+    dHdp = substitute(dHdp, Dict([f_R2s(T2s, B1, ω1, TRF) => R2s]))
 
     f_expr = build_function(dHdp, ω1, B1, ω0, T, m0s, R1f, R2f, Rex, R1s, R2s, dR2sdT2s, dR2sdB1, grad_type;
         force_SA=true,
@@ -44,24 +47,193 @@ for p ∈ [m0s, R1f, R2f, Rex, R1s, T2s, B1, ω0, R1a]
     f_str = f_str[1:idcs[end]] * "::grad_$p" * f_str[idcs[end]+1:end]
 
     idcs = findfirst("(SymbolicUtils.Code.create_array)(StaticArraysCore.SArray, nothing, Val{2}(), Val{($(size(dHdp, 1)), $(size(dHdp, 2)))}(), ", f_str)
-
     f_str = f_str[1:idcs[1]-1] * "SMatrix{$(size(dHdp, 1)), $(size(dHdp, 2))}(" * f_str[idcs[end]+1:end]
     global fs_str *= f_str
-    global fs_str *= "\n \n"
+    global fs_str *= "\n"
 end
 
-##
-# Find all start and end markers
+## #########################################################################################
+# derivatives wrt. ω1 (used for optimal control)
+############################################################################################
+f_dR2sdω1(T2s, B1) = dR2sdω1
+@register_symbolic f_dR2sdω1(T2s, B1)
+
+@register_derivative f_R2s(T2s, B1, ω1, TRF) 3 f_dR2sdω1(T2s, B1)
+@register_derivative f_dR2sdω1(T2s, B1) 1 SConst(dR2sdT2sdω1)
+@register_derivative f_dR2sdω1(T2s, B1) 2 SConst(dR2sdB1dω1)
+
+Ḣ = expand_derivatives.(Differential(ω1).(H))
+Ḣ = substitute(Ḣ, Dict([f_dR2sdω1(T2s, B1) => dR2sdω1]))
+
+# no grad_type
+f_expr = build_function(Matrix(Ḣ), B1, T, dR2sdω1, dR2sdT2sdω1, dR2sdB1dω1, grad_type;
+    force_SA=true,
+)
+
+f_str = string(f_expr[1])
+f_str = f_str[1:9] * "d_hamiltonian_linear_dω1" * f_str[10:end]
+
+idcs = findfirst("dR2sdT2sdω1", f_str)
+f_str = f_str[1:idcs[end]] * "=nothing" * f_str[idcs[end]+1:end]
+idcs = findfirst("dR2sdB1dω1", f_str)
+f_str = f_str[1:idcs[end]] * "=nothing" * f_str[idcs[end]+1:end]
+idcs = findfirst("grad_type", f_str)
+f_str = f_str[1:idcs[end]] * "=nothing" * f_str[idcs[end]+1:end]
+
+idcs = findfirst("(SymbolicUtils.Code.create_array)(StaticArraysCore.SArray, nothing, Val{2}(), Val{($(size(Ḣ, 1)), $(size(Ḣ, 2)))}(), ", f_str)
+f_str = f_str[1:idcs[1]-1] * "SMatrix{$(size(Ḣ, 1)), $(size(Ḣ, 2))}(" * f_str[idcs[end]+1:end]
+fs_str *= f_str
+fs_str *= "\n"
+
+
+# grad_type::grad_param (generic)
+dHdp = vcat(
+    hcat(Ḣ[1:end-1, 1:end-1], Z[1:end-1, 1:end-1], Ḣ[1:end-1, end]),
+    hcat(Z[1:end-1, 1:end-1], Ḣ[1:end-1, 1:end-1], Ḣ[1:end-1, end]),
+    zeros(Int, 1, 2size(H, 2) - 1)
+)
+
+f_expr = build_function(dHdp, B1, T, dR2sdω1, dR2sdT2sdω1, dR2sdB1dω1, grad_type;
+    force_SA=true,
+)
+
+f_str = string(f_expr[1])
+f_str = f_str[1:9] * "d_hamiltonian_linear_dω1" * f_str[10:end]
+
+idcs = findfirst("grad_type", f_str)
+f_str = f_str[1:idcs[end]] * "::grad_param" * f_str[idcs[end]+1:end]
+
+idcs = findfirst("(SymbolicUtils.Code.create_array)(StaticArraysCore.SArray, nothing, Val{2}(), Val{($(size(dHdp, 1)), $(size(dHdp, 2)))}(), ", f_str)
+f_str = f_str[1:idcs[1]-1] * "SMatrix{$(size(dHdp, 1)), $(size(dHdp, 2))}(" * f_str[idcs[end]+1:end]
+
+fs_str *= f_str
+fs_str *= "\n"
+
+
+# grad_type::grad_B1 and grad_type::grad_T2s
+for p ∈ [B1, T2s]
+    Ḣ = expand_derivatives.(Differential(ω1).(H))
+    dḢdp = expand_derivatives.(Differential(p).(Ḣ))
+
+    dHdp = vcat(
+        hcat(Ḣ[1:end-1, 1:end-1], Z[1:end-1, 1:end-1], Ḣ[1:end-1, end]),
+        hcat(dḢdp[1:end-1, 1:end-1], Ḣ[1:end-1, 1:end-1], Ḣ[1:end-1, end]),
+        zeros(Int, 1, 2size(H, 2) - 1)
+    )
+    dHdp = substitute(dHdp, Dict([f_dR2sdω1(T2s, B1) => dR2sdω1]))
+
+    f_expr = build_function(dHdp, B1, T, dR2sdω1, dR2sdT2sdω1, dR2sdB1dω1, grad_type;
+        force_SA=true,
+    )
+
+    f_str = string(f_expr[1])
+    f_str = f_str[1:9] * "d_hamiltonian_linear_dω1" * f_str[10:end]
+
+    idcs = findfirst("grad_type", f_str)
+    f_str = f_str[1:idcs[end]] * "::grad_$p" * f_str[idcs[end]+1:end]
+
+    idcs = findfirst("(SymbolicUtils.Code.create_array)(StaticArraysCore.SArray, nothing, Val{2}(), Val{($(size(dHdp, 1)), $(size(dHdp, 2)))}(), ", f_str)
+    f_str = f_str[1:idcs[1]-1] * "SMatrix{$(size(dHdp, 1)), $(size(dHdp, 2))}(" * f_str[idcs[end]+1:end]
+
+    global fs_str *= f_str
+    global fs_str *= "\n"
+end
+
+## #########################################################################################
+# derivatives wrt. TRF (used for optimal control)
+############################################################################################
+f_dR2sdTRF(T2s, B1) = dR2sdTRF
+@register_symbolic f_dR2sdTRF(T2s, B1)
+
+@register_derivative f_R2s(T2s, B1, ω1, TRF) 4 f_dR2sdTRF(T2s, B1)
+@register_derivative f_dR2sdTRF(T2s, B1) 1 SConst(dR2sdT2sdTRF)
+@register_derivative f_dR2sdTRF(T2s, B1) 2 SConst(dR2sdB1dTRF)
+
+Ḣ = expand_derivatives.(Differential(TRF).(H))
+Ḣ = substitute(Ḣ, Dict([f_dR2sdTRF(T2s, B1) => dR2sdTRF]))
+
+# no grad_type
+f_expr = build_function(Matrix(Ḣ), T, dR2sdTRF, dR2sdT2sdTRF, dR2sdB1dTRF, grad_type;
+    force_SA=true,
+)
+
+f_str = string(f_expr[1])
+f_str = f_str[1:9] * "d_hamiltonian_linear_dTRF_add" * f_str[10:end]
+
+idcs = findfirst("dR2sdT2sdTRF", f_str)
+f_str = f_str[1:idcs[end]] * "=nothing" * f_str[idcs[end]+1:end]
+idcs = findfirst("dR2sdB1dTRF", f_str)
+f_str = f_str[1:idcs[end]] * "=nothing" * f_str[idcs[end]+1:end]
+idcs = findfirst("grad_type", f_str)
+f_str = f_str[1:idcs[end]] * "=nothing" * f_str[idcs[end]+1:end]
+
+idcs = findfirst("(SymbolicUtils.Code.create_array)(StaticArraysCore.SArray, nothing, Val{2}(), Val{($(size(Ḣ, 1)), $(size(Ḣ, 2)))}(), ", f_str)
+f_str = f_str[1:idcs[1]-1] * "SMatrix{$(size(Ḣ, 1)), $(size(Ḣ, 2))}(" * f_str[idcs[end]+1:end]
+fs_str *= f_str
+fs_str *= "\n \n"
+
+
+# grad_type::grad_param (generic)
+dHdp = vcat(
+    hcat(Ḣ[1:end-1, 1:end-1], Z[1:end-1, 1:end-1], Ḣ[1:end-1, end]),
+    hcat(Z[1:end-1, 1:end-1], Ḣ[1:end-1, 1:end-1], Ḣ[1:end-1, end]),
+    zeros(Int, 1, 2size(H, 2) - 1)
+)
+
+f_expr = build_function(dHdp, T, dR2sdTRF, dR2sdT2sdTRF, dR2sdB1dTRF, grad_type;
+    force_SA=true,
+)
+
+f_str = string(f_expr[1])
+f_str = f_str[1:9] * "d_hamiltonian_linear_dTRF_add" * f_str[10:end]
+
+idcs = findfirst("grad_type", f_str)
+f_str = f_str[1:idcs[end]] * "::grad_param" * f_str[idcs[end]+1:end]
+
+idcs = findfirst("(SymbolicUtils.Code.create_array)(StaticArraysCore.SArray, nothing, Val{2}(), Val{($(size(dHdp, 1)), $(size(dHdp, 2)))}(), ", f_str)
+f_str = f_str[1:idcs[1]-1] * "SMatrix{$(size(dHdp, 1)), $(size(dHdp, 2))}(" * f_str[idcs[end]+1:end]
+
+fs_str *= f_str
+fs_str *= "\n"
+
+
+# grad_type::grad_B1 and grad_type::grad_T2s
+for p ∈ [B1, T2s]
+    Ḣ = expand_derivatives.(Differential(TRF).(H))
+    dḢdp = expand_derivatives.(Differential(p).(Ḣ))
+
+    dHdp = vcat(
+        hcat(   Ḣ[1:end-1, 1:end-1], Z[1:end-1, 1:end-1], Ḣ[1:end-1, end]),
+        hcat(dḢdp[1:end-1, 1:end-1], Ḣ[1:end-1, 1:end-1], Ḣ[1:end-1, end]),
+        zeros(Int, 1, 2size(H, 2) - 1)
+    )
+    dHdp = substitute(dHdp, Dict([f_dR2sdTRF(T2s, B1) => dR2sdTRF]))
+
+    f_expr = build_function(dHdp, T, dR2sdTRF, dR2sdT2sdTRF, dR2sdB1dTRF, grad_type;
+        force_SA=true,
+    )
+
+    f_str = string(f_expr[1])
+    f_str = f_str[1:9] * "d_hamiltonian_linear_dTRF_add" * f_str[10:end]
+
+    idcs = findfirst("grad_type", f_str)
+    f_str = f_str[1:idcs[end]] * "::grad_$p" * f_str[idcs[end]+1:end]
+
+    idcs = findfirst("(SymbolicUtils.Code.create_array)(StaticArraysCore.SArray, nothing, Val{2}(), Val{($(size(dHdp, 1)), $(size(dHdp, 2)))}(), ", f_str)
+    f_str = f_str[1:idcs[1]-1] * "SMatrix{$(size(dHdp, 1)), $(size(dHdp, 2))}(" * f_str[idcs[end]+1:end]
+
+    global fs_str *= f_str
+    global fs_str *= "\n"
+end
+
+## #########################################################################################
+# remove cryptic and changing comments
+############################################################################################
 starts = findall("#=", fs_str)
 ends   = findall("=#", fs_str)
-
-# Sanity check: number of starts and ends should match
 @assert length(starts) == length(ends) "Mismatched #= and =# markers"
 
-# Start with the part before the first block comment
 fsc_str = fs_str[1:starts[1][1]-1]
-
-# Append all non-comment regions
 for i in 1:length(starts)
     from = ends[i][2] + 1
     to = i == length(starts) ? lastindex(fs_str) : starts[i+1][1] - 1
@@ -71,6 +243,5 @@ end
 # remove whitespace lines
 fsc_str = join(filter(x -> !isempty(strip(x)), split(fsc_str, '\n')), "\n")
 
-
-##
+## #########################################################################################
 write("src/MatrixExp_Hamiltonian_Gradients.jl", fsc_str)
